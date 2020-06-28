694beb0313103412b7adc907994fbefe737d5507
Fabio Delgado <fabio.delgado2@gmail.com>
2020-06-28 14:18:29 -0300

    ajuste file upload async

--- Controllers/FileUploadController.cs
+++ Controllers/FileUploadController.cs
@@ -9,17 +9,17 @@ using CadastroApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace cadastro.Controllers {
     [Route ("api/[controller]")]
     [Authorize ()]
     [ApiController]
     public class FileUploadController : Controller {
-        private readonly ClienteRepository _repository;
 
-        public FileUploadController (ClienteRepository repository) {
-            this._repository = repository ??
-                throw new ArgumentNullException (nameof (repository));
+        private readonly IConfiguration _config;
+        public FileUploadController (IConfiguration config) {
+             _config = config;
         }
 
         [HttpPost, DisableRequestSizeLimit]
@@ -27,8 +27,8 @@ namespace cadastro.Controllers {
         public async Task<string> Post () {
             try {
                 foreach (var file in Request.Form.Files) {
-                    string folderName = "Upload";
-                    string webRootPath = "C:/Prod/";
+                    string folderName = _config["FileUpload:folderName"];
+                    string webRootPath = _config["FileUpload:webRootPath"];
                     string newPath = Path.Combine (webRootPath, folderName);
                     if (!Directory.Exists (newPath)) {
                         Directory.CreateDirectory (newPath);
@@ -37,10 +37,9 @@ namespace cadastro.Controllers {
                         string fileName = file.FileName.Trim ('"');
                         string fullPath = Path.Combine (newPath, fileName);
                         using (var stream = new FileStream (fullPath, FileMode.Create)) {
-                            file.CopyTo (stream);
+                            await file.CopyToAsync (stream);
                         }
                     }
-
                 }
                 return "Upload Successful";
             } catch (System.Exception ex) {
